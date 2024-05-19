4ef3e65d8d5795516b862130684587d5d4fee485
rockingerser <100497322+rockingerser@users.noreply.github.com>
2024-05-19 10:35:52 -0500

    fix

--- lua/modules/test.lua
+++ lua/modules/test.lua
@@ -1,14 +1,14 @@
 local module = {}
 
-print "Thanks for requiring me!"
+print("Thanks for requiring me!")
 
 function module.Test1()
-    print "Hello from module!"
+    print("Hello from module!")
 end
 
 function module.Sum(a, b)
     local result = a + b
-    print "The result is "..result
+    print("The result is "..result)
 end
 
 function module.GetPI()
