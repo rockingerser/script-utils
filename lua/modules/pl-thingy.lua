e0f5c3de7cbd6d8018873c09a2a2ce9b0f0c7f3f
rockingerser <100497322+rockingerser@users.noreply.github.com>
2024-06-20 08:06:04 -0500

    update

--- lua/modules/pl-thingy.lua
+++ lua/modules/pl-thingy.lua
@@ -1014,7 +1014,7 @@ function CharacterAdded(NewCharacter)
 
 
 	local function ToolSoundAdded(Sound)
-		--[[if Humanoid:GetState() ~= Enum.HumanoidStateType.Dead and table.find(MySounds, Sound) == nil then
+		if Humanoid:GetState() ~= Enum.HumanoidStateType.Dead and table.find(MySounds, Sound) == nil and Sound.Name == "punchSound" then
 			local SoundArg = {
 				Sound,
 				Sound:FindFirstAncestorOfClass("Tool")
@@ -1022,7 +1022,7 @@ function CharacterAdded(NewCharacter)
 
 			table.insert(MySounds, SoundArg)
 			table.insert(SpamSounds, SoundArg)
-		end]]
+		end
 	end
 
 	Humanoid.Died:Once(function()
