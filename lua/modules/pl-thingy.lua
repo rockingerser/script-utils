d5fd8493efe82a6afa8e0df52b32570085c46b13
rockingerser <100497322+rockingerser@users.noreply.github.com>
2024-06-21 16:25:26 -0500

    update

--- lua/modules/pl-thingy.lua
+++ lua/modules/pl-thingy.lua
@@ -1055,6 +1055,7 @@ function CharacterAdded(NewCharacter)
 
 
 	local function ToolSoundAdded(Sound)
+		print(Sound)
 		if Humanoid:GetState() ~= Enum.HumanoidStateType.Dead and table.find(MySounds, Sound) == nil and Sound.Name == "Died" then
 			local Tool = Sound:FindFirstAncestorOfClass("Tool")
 
